0da00d77562aeaed0e3fadaa3c681e3d4cb0bb52
Felipe Madureira de Souza <Felippefms3@gmail.com>
2026-03-19 20:36:11 -0300

    [add] i18n in contact page

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -6,18 +6,21 @@ import telefoneimg from "@/media/images/phone-call.png";
 import emailimg from "@/media/images/gmail.png";
 import githubimg from "@/media/images/github.png";
 import linkedinimg from "@/media/images/linkedin.png";
+import { useTranslation } from 'react-i18next';
 
 export const Route = createFileRoute('/contact')({
   component: Contact,
 })
 
 function Contact() {
+  const { t } = useTranslation()
+
   return (
     <PageWrapper>
-        <ContactBox text="Telefone" image={telefoneimg} content='https://wa.me/+05527999053721' itemtext='(27) 99905-3721'></ContactBox>
-        <ContactBox text="E-Mail" image={emailimg} content='https://mail.google.com/mail/u/0/?fs=1&to=felippefms3@gmail.com&tf=cm' itemtext='felippefms3@gmail.com'></ContactBox>
-        <ContactBox text="GitHub" image={githubimg} content='https://github.com/felippefms' itemtext='github.com/felippefms' target="_blank"></ContactBox>
-        <ContactBox text="LinkedIn" image={linkedinimg} content='https://linkedin.com/in/felippefms' itemtext='linkedin.com/in/felippefms'></ContactBox>
+      <ContactBox text={t('contact_phone')} image={telefoneimg} content='https://wa.me/+05527999053721' itemtext={t('contact_phone_number')} />
+      <ContactBox text={t('contact_email')} image={emailimg} content='https://mail.google.com/mail/u/0/?fs=1&to=felippefms3@gmail.com&tf=cm' itemtext='felippefms3@gmail.com' />
+      <ContactBox text={t('contact_github')} image={githubimg} content='https://github.com/felippefms' itemtext='github.com/felippefms' target="_blank" />
+      <ContactBox text={t('contact_linkedin')} image={linkedinimg} content='https://linkedin.com/in/felippefms' itemtext='linkedin.com/in/felippefms' />
     </PageWrapper>
   )
 }
